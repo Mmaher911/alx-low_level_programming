5d1345b075249cb67b8b6885cfd0915b41cfbf4e
Mmaher911 <mohamed.saidco@gmail.com>
2023-10-08 15:41:32 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * description: print all alphabet letters
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
