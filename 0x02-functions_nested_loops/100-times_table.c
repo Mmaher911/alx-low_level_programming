855ccf135531367f06abb4c9a62107e73c906201
Mmaher911 <mohamed.saidco@gmail.com>
2023-10-10 16:46:52 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,8 +32,8 @@ void print_times_table(int n)
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) % 10 + 48);
 				} else if (prod <= 99 && prod >= 10)
-					_putcahr((prod / 10) + 48);
-				_putcahr((prod % 10) + 48);
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
