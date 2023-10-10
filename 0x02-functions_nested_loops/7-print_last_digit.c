31c9de7b86a00bc4ed4ff158f52ed2d1f3fad123
Mmaher911 <mohamed.saidco@gmail.com>
2023-10-10 14:55:08 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,7 @@
  *
  * Return: lastDigit
 */
-
+i
 int print_last_digit(int n)
 {
 	int lastDigit;
@@ -17,6 +17,6 @@ int print_last_digit(int n)
 	else
 		lastDigit = n % 10;
 
-	_putchar(lastDigit = '0');
+	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
