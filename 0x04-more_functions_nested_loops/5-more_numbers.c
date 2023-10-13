e04fa62ac308c9453e5dd36e72562939a21eb3cc
Mmaher911 <mohamed.saidco@gmail.com>
2023-10-12 17:31:36 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - print 0 - 14 ten times
+ * more_numbers - print 0 - 14 ten times and you
+ *	can only use _putchar three times
  *
  * Return: Always 0 (Success)
 */
