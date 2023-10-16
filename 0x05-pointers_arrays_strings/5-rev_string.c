20de56b7eba35a62777b9d24044c7a3aa243d6b0
Mmaher911 <mohamed.saidco@gmail.com>
2023-10-16 14:15:17 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string  - reverce string
+ *
+ * @s: pointer to the string to print
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	/*find string lengthwithou null char*/
+	for (l = 0; s[l] != '\0'; ++1)
+		;
+
+	/*swap the string to half */
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i]; /*-1 because the array start from 0*/
+		s[l - 1 - i] = temp;
+	}
+	_putchar('\n');
+}
