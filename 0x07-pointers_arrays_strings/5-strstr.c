b1cec5185b31766ac764fcb212e1587b22d99742
Mmaher911 <mohamed.saidco@gmail.com>
2023-10-23 05:50:09 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strstr - 
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: pointer s
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int itr;
+
+	for (itr = 0; n > 0; itr++, n--)
+	{
+		s[itr] = b;
+	}
+
+	return (s);
+}
