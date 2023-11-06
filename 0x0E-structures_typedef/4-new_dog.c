3ecd3b70d51a3a38007dd11a5af1c50fd48be393
Mmaher911 <mohamed.saidco@gmail.com>
2023-11-06 12:25:32 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,7 +26,7 @@ int _strlen(const char *str)
  *
  */
 
-char *_strcpy(char *dest, char *src)
+char *_strcopy(char *dest, char *src)
 {
 	int i;
 
@@ -50,7 +50,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	/* if name and owner are empty and age is less than zero return null*/
-
 	if (!name || age < 0 || !owner)
 		return (NULL);
 
